c8abc4610ad475f96faee0f8843153f452335e3e
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-05-24 15:49:30 +0300

    fix: use remote branch when determining tags
    The local might be out of date

--- generateNotes.js
+++ generateNotes.js
@@ -2,7 +2,7 @@ const changelog = require('conventional-changelog')
 const parseUrl = require('github-url-from-git')
 const execSync = require('child_process').execSync;
 
-const describe = () => execSync('git describe --tags master', { encoding: 'utf8' });
+const describe = () => execSync('git describe --tags origin/master', { encoding: 'utf8' });
 const baseTag = (tag) => tag.replace(/(v[0-9.]+)(.*)/, '$1').trim();
 const lastTag = () => baseTag(describe());
 
