5989217c89ef42d7bd8b3c5ce69404fee5eade36
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2017-05-26 10:19:18 +0300

    refactor: trim tag name in git-describe

--- generateNotes.js
+++ generateNotes.js
@@ -2,9 +2,7 @@ const changelog = require('conventional-changelog')
 const parseUrl = require('github-url-from-git')
 const execSync = require('child_process').execSync;
 
-const describe = () => execSync('git describe --tags origin/master', { encoding: 'utf8' });
-const baseTag = (tag) => tag.replace(/(v[0-9.]+)(.*)/, '$1').trim();
-const lastTag = () => baseTag(describe());
+const lastTag = () => execSync('git describe --tags --abbrev=0 origin/master', { encoding: 'utf8' }).trim();
 
 module.exports = function (pluginConfig, {pkg}, cb) {
   const repository = pkg.repository ? parseUrl(pkg.repository.url) : null
