e767a25ee6efffa60f06d9e7568dfd95c9866fb7
Ivan Peshev <ivan.g.peshev@gmail.com>
2020-07-16 14:06:40 +0300

    feat: generate theme section decorator

--- generateNotes.js
+++ generateNotes.js
@@ -5,7 +5,10 @@ const lastTag = require('./lastTag');
 module.exports = function (pluginConfig, {pkg}, cb) {
   const repository = pkg.repository ? parseUrl(pkg.repository.url) : null
   const from = lastTag();
-
+  const themeVersionSectionDecorator = function() {
+    //if( theme change since last master ) generate and add required theme section
+    cb.apply(null, arguments);
+  }
   changelog({
     version: pkg.version,
     repository: repository,
