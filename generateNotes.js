695a0b0961ede564d5998cd24149023dc5db9727
Ivan Peshev <ivan.g.peshev@gmail.com>
2020-07-21 13:32:38 +0300

    feat: add required theme decoration

--- generateNotes.js
+++ generateNotes.js
@@ -1,12 +1,21 @@
 const changelog = require('conventional-changelog')
 const parseUrl = require('github-url-from-git')
 const lastTag = require('./lastTag');
-
+const packageJson = require('../../../package.json');
 module.exports = function (pluginConfig, {pkg}, cb) {
   const repository = pkg.repository ? parseUrl(pkg.repository.url) : null
   const from = lastTag();
   const themeVersionSectionDecorator = function() {
-    //if( theme change since last master ) generate and add required theme section
+    let log = arguments[1];
+    log += "#### Themes required\n\n";
+    const devDeps = packageJson.devDependencies;
+    for (const pack in devDeps) {
+        if (pack.match(/@progress\/kendo-theme-/)) {
+            log += `* ${pack}: ${devDeps[pack]}\n`
+        }
+    }
+    log += "\n";
+    arguments[1] = log;
     cb.apply(null, arguments);
   }
   changelog({
@@ -14,6 +23,6 @@ module.exports = function (pluginConfig, {pkg}, cb) {
     repository: repository,
     from: from,
     file: false
-  }, cb)
+  }, themeVersionSectionDecorator)
 };
 
