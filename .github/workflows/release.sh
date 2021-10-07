4ef387b8f8f31098720cf61eaa0e0018ed1182f4
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2021-10-07 09:48:30 +0300

    chore: print newlines in errors

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -3,11 +3,10 @@
 ERROR=$(npm run semantic-release 2>&1)
 EXIT_CODE=$?
 
-echo $ERROR;
+echo "$ERROR"
 
 if [ $EXIT_CODE -eq 1 ] && [[ $ERROR != *"ENOCHANGE"* ]]; then
   exit 1;
 else
   exit 0;
 fi
-
