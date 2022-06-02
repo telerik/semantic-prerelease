fd160d9483fcecc52a314bc9c74d84ac8b6eb9b8
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-06-02 13:17:46 +0300

    refactor: update eslint

--- condition-github-actions.js
+++ condition-github-actions.js
@@ -3,7 +3,7 @@ var SRError = require('@semantic-release/error')
 module.exports = function (pluginConfig, config, cb) {
   var env = config.env
 
-  if (!env.hasOwnProperty('GITHUB_ACTION')) {
+  if (!Object.hasOwnProperty.call(env, 'GITHUB_ACTION')) {
     return cb(new SRError(
       'semantic-release didn’t run on Github Action  and therefore a new version won’t be published.\n' +
       'You can customize this behavior using "verifyConditions" plugins: git.io/sr-plugins',
--- verifyRelease.js
+++ verifyRelease.js
@@ -2,7 +2,7 @@ module.exports = function (pluginConfig, config, cb) {
   if (config.nextRelease) {
     // change version suffix of pre-releases from '-number' to '-tag.timestamp'
     const now = new Date();
-    const timestamp = now.toISOString().replace(/[\-T\:]/g, "").slice(0,12);
+    const timestamp = now.toISOString().replace(/[-T:]/g, "").slice(0,12);
     const suffix = `-${config.npm.tag}.${timestamp}`;
     const release = config.nextRelease;
 
@@ -10,7 +10,7 @@ module.exports = function (pluginConfig, config, cb) {
       // start releases with 0.1.0 instead of 1.0.0
       release.version = '0.1.0';
     } else {
-      release.version = release.version.replace(/\-.+$/, suffix);
+      release.version = release.version.replace(/-.+$/, suffix);
     }
 
     console.log(`Ready for release v${release.version}`);
