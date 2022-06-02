d8df6d48765f5f626a25d8b73dc14da8ffdb3e19
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-06-02 13:22:37 +0300

    feat: drop support for Travis CI and Jenkins
    BREAKING CHANGE: Only compatible with GH Actions

--- verifyConditions.js
+++ verifyConditions.js
@@ -1,17 +1,8 @@
 const utils = require('./utils');
 
 module.exports = function (pluginConfig, config, cb) {
-  let defaultVerifyConditions;
-
-  if (config.env.TRAVIS) {
-    defaultVerifyConditions = require('@semantic-release/condition-travis');
-  } else if (config.env.GITHUB_REF) {
-    defaultVerifyConditions = require('./condition-github-actions');
-  } else {
-    defaultVerifyConditions = require('@krux/condition-jenkins');
-  }
-  
-  const branch = config.env.TRAVIS_BRANCH || config.env.GIT_LOCAL_BRANCH ||  utils.ghActionsBranch(config.env);
+  let verifyConditions = require('./condition-github-actions');
+  const branch = utils.ghActionsBranch(config.env);
 
   // update semantic-release configuration to publish:
   // - from this branch
@@ -25,5 +16,5 @@ module.exports = function (pluginConfig, config, cb) {
   }
 
   // run default build checks with the new configuration
-  return defaultVerifyConditions(pluginConfig, config, cb);
+  return verifyConditions(pluginConfig, config, cb);
 };
